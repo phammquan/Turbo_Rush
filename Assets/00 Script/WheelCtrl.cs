eca9f4d7f4edde0c5fc32f41726300f0045daf12
phammquan <phamminhq91@gmail.com>
2025-02-18 08:45:57 +0700

    done wheel ctrl

---
+++ Assets/00 Script/WheelCtrl.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WheelCtrl : MonoBehaviour
+{
+
+    void Update()
+    {
+        transform.Rotate(Vector3.back, 200 * Time.deltaTime);        
+    }
+}
