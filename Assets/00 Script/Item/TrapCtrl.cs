67e2d35d24f01f07e1b52a3672378da64452934e
phammquan <phamminhq91@gmail.com>
2025-02-28 12:14:46 +0700

    fix interface Trap

--- Assets/00 Script/Item/TrapCtrl.cs
+++ Assets/00 Script/Item/TrapCtrl.cs
@@ -3,10 +3,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TrapCtrl : MonoBehaviour
+public class TrapCtrl : MonoBehaviour, ITrap
 {
     [SerializeField] float _velocity;
     [SerializeField] GameObject _vfx;
+    [SerializeField] private float _force;
+    private int _damage = 10;
 
     private void OnEnable()
     {
@@ -35,25 +37,52 @@ public class TrapCtrl : MonoBehaviour
         if (other.gameObject.CompareTag("Player"))
         {
             SoundController.Instance.SFXPlay("Hit");
-            ITakeDamge _isCanTakeDmg = other.gameObject.GetComponent<ITakeDamge>();
-            if(_isCanTakeDmg != null)   
-            {
-                _isCanTakeDmg.TakeDamage(10);
-            }
-            foreach (ContactPoint contact in other.contacts)
-            {
-                Vector3 collisionPoint = contact.point;
-                GameObject vfxInstance = Object_Pooling.Instance.GetPrefabs(_vfx);
-                if (vfxInstance != null)
-                {
-                    vfxInstance.transform.position = collisionPoint;
-                    vfxInstance.SetActive(true);
-                }
-                else
-                {
-                    Debug.LogWarning("VFX instance is null. Check Object Pooling setup.");
-                }
-            }
+            Damge(other);
+            Force(other);
+            VFX(other);
+            
         }        
+    }
+
+
+    public void Force(Collision others)
+    {
+        
+        Rigidbody playerRb = others.gameObject.GetComponent<Rigidbody>();
+        if (playerRb != null)
+        {
+            Vector3 direction = others.contacts[0].point - transform.position;
+            direction.y = 0; 
+            direction.Normalize();
+            
+            playerRb.AddForce(direction * _force, ForceMode.Impulse);
+        }
+    }
+
+    public void VFX(Collision others)
+    {
+        foreach (ContactPoint contact in others.contacts)
+        {
+            Vector3 collisionPoint = contact.point;
+            GameObject vfxInstance = Object_Pooling.Instance.GetPrefabs(_vfx);
+            if (vfxInstance != null)
+            {
+                vfxInstance.transform.position = collisionPoint;
+                vfxInstance.SetActive(true);
+            }
+            else
+            {
+                Debug.LogWarning("VFX instance is null. Check Object Pooling setup.");
+            }
+        }
+    }
+
+    public void Damge(Collision others)
+    {
+        ITakeDamge _isCanTakeDmg = others.gameObject.GetComponent<ITakeDamge>();
+        if(_isCanTakeDmg != null)   
+        {
+            _isCanTakeDmg.TakeDamage(_damage);
+        }
     }
 }
